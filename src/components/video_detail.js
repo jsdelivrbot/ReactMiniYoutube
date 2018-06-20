4d77e88a8154ca65ef447a3ec3b6e305163542b3
Laurens Chaves Méndez <laurens004@gmail.com>
2018-06-19 20:28:38 -0600

    StylingCSS

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -6,17 +6,20 @@ const VideoDetail = ({video}) => {
     return <div>Loading...</div>;
   }
 
+  console.log(video);
+  console.log(video.snippet.title);
+
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
   return (
-    <div className="video-detail col-md-8">
+    <div className="video-details col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}>
 
         </iframe>
       </div>
       <div className="details">
-        <div>{video.snippet.tittle}</div>
+        <div>{video.snippet.title}</div>
         <div>{video.snippet.description}</div>
       </div>
     </div>
